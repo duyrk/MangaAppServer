d9fc208b06d426ecb8f7bd15d79ed93e57d68b5b
duy vo <duyvo761@gmail.com>
2024-03-05 17:22:30 +0700

    comment view engine

--- app.js
+++ app.js
@@ -29,8 +29,8 @@ var mangaCPanelRouter = require("./routes/CPanel/Manga");
 var userCPanelRouter = require("./routes/CPanel/User");
 db.connect();
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "views"));
+// app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
